874217a2c3bac8124524ebd05c69b38c8a8424d5
Jennifer <jennifer.k.silva7@aluno.senai.br>
2025-09-15 11:37:07 -0300

    feat: Cria arquivo Inserts da tabela Time-Brasil e adiciona valores iniciais

---
+++ atividade-01/time-Brasil-selects.sql
@@ -0,0 +1,9 @@
+SELECT * FROM times_brasil;
+
+SELECT COUNT(*) AS total_times FROM times_brasil;
+
+SELECT nome, rebaixado FROM times_brasil
+WHERE rebaixado = 'FALSE';
+
+SELECT nome,numero_titulos FROM times_brasil
+WHERE id = 1 
