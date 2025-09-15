4ced6839b687575724b0a3e938dbc78251985aa1
Jennifer <jennifer.k.silva7@aluno.senai.br>
2025-09-15 10:50:46 -0300

    feat: Finaliza selects

--- queries/02-selects.sql
+++ queries/02-selects.sql
@@ -1,3 +1,9 @@
-SELECT * FROM artistas 
+SELECT * FROM artistas;
 
 SELECT COUNT(*) AS total_artistas FROM artistas;
+
+SELECT nome, musica_mais_ouvida FROM artistas
+WHERE pais_de_origem = 'Brasil';
+
+SELECT nome, musica_mais_ouvida FROM artistas
+WHERE id = 5 
