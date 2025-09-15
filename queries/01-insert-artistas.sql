edbe45763b6bec2e8f6d0ece8cfb54abf29183e3
Jennifer <jennifer.k.silva7@aluno.senai.br>
2025-09-15 09:56:23 -0300

    feat:Adiciona os primeiros valores

--- queries/01-insert-artistas.sql
+++ queries/01-insert-artistas.sql
@@ -9,3 +9,8 @@ CREATE TABLE artistas (
     musica_mais_ouvida VARCHAR(100),
     ativo BOOLEAN DEFAULT TRUE
 );
+
+INSERT INTO artistas (nome, tipo, membros, genero_musical, pais_de_origem, ano_inicio, musica_mais_ouvida, ativo)
+VALUES 
+('MORADA','Banda',4,'Gospel','Brasil',2009,'Só Tu És Santo / Uma Coisa / Deixa Queimar / Quando Ele Vem',TRUE)
+;
